862bd06e5e43f3bd648eafc05498b2498247fe93
Vic-L <vljc17@gmail.com>
2015-10-25 01:15:37 +0800

    generate devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { 
+    sessions: "users/sessions",
+    registrations: "users/registrations" }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'pages#home'
 

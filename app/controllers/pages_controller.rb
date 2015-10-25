d746eabdc574d38d471f9b0ac29515dbdf993629
Vic-L <vljc17@gmail.com>
2015-10-25 11:29:43 +0800

    checkpoint fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,12 @@
 class PagesController < ActionController::Base
+  layout :layout_for_page
+
+  private
+    def layout_for_page
+      case params[:id]
+      when 'lol'
+      else
+        'application'
+      end
+    end  
 end

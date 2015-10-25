5f82dfb525306b7cbe638ed1bcf497d380f91eef
Vic-L <vljc17@gmail.com>
2015-10-25 14:14:22 +0800

    successful sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+  def sign_up
+    default_params.permit(:name, :email, :company_name, :password, :password_confirmation)
+  end
+end

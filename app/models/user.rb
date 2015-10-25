003d9cb77bba9283db85ff789a7b86ec86ed80db
Vic-L <vljc17@gmail.com>
2015-10-25 14:23:59 +0800

    successful login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User::ParameterSanitizer < Devise::ParameterSanitizer
   def sign_up
     default_params.permit(:name, :email, :company_name, :password, :password_confirmation)
   end
+  
+  def sign_in
+    default_params.permit(:email, :password)
+  end
 end

2078093ce882390ef32eb50bb9a7740bc27792d4
Vic-L <vljc17@gmail.com>
2015-10-26 12:03:46 +0800

    redirect authenticated users away from home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ActionController::Base
   layout :layout_for_page
+  before_action :authenticate_current_user
+
+  def home
+
+  end
 
   private
     def layout_for_page
@@ -8,5 +13,11 @@ class PagesController < ActionController::Base
       else
         'application'
       end
-    end  
+    end
+
+    def authenticate_current_user
+      if user_signed_in?
+        redirect_to current_tenders_path
+      end
+    end
 end

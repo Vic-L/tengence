5e820397b68b04d0110b9182f615d819444f76f7
Vic-L <vljc17@gmail.com>
2015-10-26 12:03:25 +0800

    display current_tenders results nicely

--- app/models/current_tender.rb
+++ app/models/current_tender.rb
@@ -2,4 +2,8 @@ class CurrentTender < ActiveRecord::Base
   self.table_name = 'past_tenders'
   after_initialize :readonly!
   paginates_per 50
+
+  def buyer_name_and_company
+    "#{self.buyer_name} (#{self.buyer_company_name})"
+  end
 end

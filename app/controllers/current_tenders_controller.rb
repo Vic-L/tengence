07b76b066db9cb75116b3010896b5f02a604291a
Vic-L <vljc17@gmail.com>
2015-10-28 00:55:32 +0800

    simple query to cloud search

--- app/controllers/current_tenders_controller.rb
+++ app/controllers/current_tenders_controller.rb
@@ -2,6 +2,15 @@ class CurrentTendersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tenders = CurrentTender.order(closing_datetime: :desc).page(params[:page])
+    if params['query']
+      results = AwsManager.search(params['query'])
+      @results_count = results.hits.found
+      results_ref_nos = results.hits.hit.map do |result|
+        result.fields["ref_no"][0]
+      end
+      @tenders = CurrentTender.where(ref_no: results_ref_nos)
+    else
+      @tenders = CurrentTender.order(closing_datetime: :desc).page(params[:page])
+    end
   end
 end

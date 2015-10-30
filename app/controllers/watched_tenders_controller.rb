f4bee31c48d8daf37cf9482213a9227216c3ab9e
Vic-L <vljc17@gmail.com>
2015-10-30 13:43:08 +0800

    watched_tenders search function up

--- app/controllers/watched_tenders_controller.rb
+++ app/controllers/watched_tenders_controller.rb
@@ -1,4 +1,19 @@
 class WatchedTendersController < ApplicationController
+  def index
+    if params['query']
+      results = AwsManager.watched_tenders_search(params['query'],current_user.watched_tenders.pluck(&:ref_no))
+      @results_count = results.hits.found
+      results_ref_nos = results.hits.hit.map do |result|
+        result.fields["ref_no"][0]
+      end
+      @current_watched_tenders = CurrentTender.where(ref_no: results_ref_nos)
+      @past_watched_tenders = PastTender.where(ref_no: results_ref_nos)
+    else
+      @current_watched_tenders = CurrentTender.where(ref_no: current_user.watched_tenders.pluck(&:ref_no))
+      @past_watched_tenders = PastTender.where(ref_no: current_user.watched_tenders.pluck(&:ref_no))
+    end
+  end
+
   def create
     @watched_tender = WatchedTender.create(user_id: current_user.id, tender_id: params[:ref_no])
   end

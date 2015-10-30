85cbd7bdc3501704d83c7a939c417bfb6999787d
Vic-L <vljc17@gmail.com>
2015-10-30 19:15:15 +0800

    add whenever gem and start scheduler at 3am SGT to send keywords tenders_email to users

---
+++ config/schedule.rb
@@ -0,0 +1,29 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+require "tzinfo"
+
+def local(time)
+  TZInfo::Timezone.get("Asia/Singapore").local_to_utc(Time.parse(time))
+end
+
+every :day, :at => local('3:00am'), :tz => 'Asia/Singapore' do
+  rake "emailer:schedule_send_keywords_tenders_emails"
+end

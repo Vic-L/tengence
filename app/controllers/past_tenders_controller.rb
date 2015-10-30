44cda5ea14d9fdd7141ae3fe835ec27d72974841
Vic-L <vljc17@gmail.com>
2015-10-30 12:50:08 +0800

    remove watch column under past tenders

--- app/controllers/past_tenders_controller.rb
+++ app/controllers/past_tenders_controller.rb
@@ -2,6 +2,15 @@ class PastTendersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tenders = PastTender.order(closing_datetime: :desc).page(params[:page])
+    if params['query']
+      results = AwsManager.search(params['query'])
+      @results_count = results.hits.found
+      results_ref_nos = results.hits.hit.map do |result|
+        result.fields["ref_no"][0]
+      end
+      @tenders = PastTender.where(ref_no: results_ref_nos)
+    else
+      @tenders = PastTender.order(closing_datetime: :desc).page(params[:page])
+    end
   end
 end

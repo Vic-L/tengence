4335595153c31941bed90f9cf9dca7e65a787ad3
Vic-L <vljc17@gmail.com>
2015-10-30 18:48:55 +0800

    use X-MC-SendAt headers in emails

--- app/mailers/alerts_mailer.rb
+++ app/mailers/alerts_mailer.rb
@@ -12,6 +12,6 @@ class AlertsMailer < ApplicationMailer
 
   def alert_mail user_id
     @user = User.find(user_id)
-    mail(to: @user.email, subject: "Tengence Alerts #{Time.now.in_time_zone('Asia/Singapore').to_date.yesterday}", send_at: (Time.now.in_time_zone('Asia/Singapore') + 8.hours).utc)
+    mail(to: @user.email, subject: "Tengence Alerts #{Time.now.in_time_zone('Asia/Singapore').to_date.yesterday}", :'X-MC-SendAt' => (Time.now.in_time_zone('Asia/Singapore') + 8.hours).utc.strftime("%Y-%m-%d %H:%M:%S"))
   end
 end

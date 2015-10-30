a11ac5e554ba8d886801f1cb9a017ffea9505931
Vic-L <vljc17@gmail.com>
2015-10-30 12:50:36 +0800

    add association btw tenders and watched tenders

--- app/models/watched_tender.rb
+++ app/models/watched_tender.rb
@@ -1,5 +1,6 @@
 class WatchedTender < ActiveRecord::Base
   belongs_to :current_tender, class_name: 'CurrentTender', foreign_key: :tender_id
   belongs_to :past_tender, class_name: 'PastTender', foreign_key: :tender_id
+  belongs_to :tender, foreign_key: :tender_id
   belongs_to :user
 end

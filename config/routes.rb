2fc648f4f47faa9ede6c87578641849940cf5645
Vic-L <vljc17@gmail.com>
2015-10-31 00:03:53 +0800

    setup keywords_tenders#index view

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   get '/current_tenders', to: 'current_tenders#index', as: :current_tenders
   get '/past_tenders', to: 'past_tenders#index', as: :past_tenders
+  get '/keywords_tenders', to: 'keywords_tenders#index', as: :keywords_tenders
 
   resources :watched_tenders
 

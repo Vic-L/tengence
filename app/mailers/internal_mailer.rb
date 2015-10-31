1f6f7d9babc821bc3a10682ec0e056ae9c8b87c4
Vic-L <vljc17@gmail.com>
2015-11-01 02:57:34 +0800

    cc internal_email to vljc17@gmail.com

--- app/mailers/internal_mailer.rb
+++ app/mailers/internal_mailer.rb
@@ -1,6 +1,6 @@
 class InternalMailer < ApplicationMailer
   def notify subject, content, to_target="tengencesingapore@gmail.com"
     @subject, @content = subject, content
-    mail(from: 'notification@tengence.com.sg', to: to_target, subject: @subject)
+    mail(from: 'notification@tengence.com.sg', to: to_target, subject: @subject, cc: 'vljc17@gmail.com')
   end
 end

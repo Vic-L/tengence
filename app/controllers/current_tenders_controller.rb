a5b96299090e08cf0d52c6d9c9234a0ef41ffcf6
Vic-L <vljc17@gmail.com>
2015-11-02 12:00:05 +0800

    fix atttempt

--- app/controllers/current_tenders_controller.rb
+++ app/controllers/current_tenders_controller.rb
@@ -4,11 +4,12 @@ class CurrentTendersController < ApplicationController
   def index
     unless params['query'].blank?
       results = AwsManager.search(keyword: params['query'])
-      @results_count = results.hits.found
       results_ref_nos = results.hits.hit.map do |result|
         result.fields["ref_no"][0]
       end
-      @tenders = CurrentTender.includes(:users).where(ref_no: results_ref_nos).page(params[:page]).per(50)
+      tenders = CurrentTender.includes(:users).where(ref_no: results_ref_nos)
+      @tenders = tenders.page(params[:page]).per(50)
+      @results_count = tenders.count
     else
       @results_count = CurrentTender.count
       @tenders = CurrentTender.includes(:users).page(params[:page]).per(50)

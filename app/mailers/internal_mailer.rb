76982bf243b755c814e816e6cc8c08f6966b0283
Vic-L <vljc17@gmail.com>
2015-11-03 01:54:54 +0800

    send internal mailer of the same alert email

--- app/mailers/internal_mailer.rb
+++ app/mailers/internal_mailer.rb
@@ -1,6 +1,17 @@
 class InternalMailer < ApplicationMailer
+  default from: 'notification@tengence.com.sg'
+
   def notify subject, content, to_target="tengencesingapore@gmail.com"
     @subject, @content = subject, content
-    mail(from: 'notification@tengence.com.sg', to: to_target, subject: @subject, cc: 'vljc17@gmail.com')
+    mail(to: to_target, subject: @subject, cc: 'vljc17@gmail.com')
+  end
+
+  def alert_mail user_id, ref_nos_array, tenders_count
+    @user = User.find(user_id)
+    @count = tenders_count
+    @tenders = Tender.where(ref_no: ref_nos_array)
+    @date = Time.now.in_time_zone('Asia/Singapore').to_date.yesterday
+    mail(to: @user.email, subject: "TA #{@date} for #{@user.email}", template_path: 'alerts_mailer', template_name: 'alert_mail')
+      # , :'X-MC-SendAt' => (Time.now.in_time_zone('Asia/Singapore') + 8.hours).utc.strftime("%Y-%m-%d %H:%M:%S"))
   end
 end

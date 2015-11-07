5c5c96b611c42af183bc89c3f3072f59fce541ba
Vic-L <vljc17@gmail.com>
2015-11-07 14:27:49 +0800

    change subject of emailers if sent on monday

--- app/mailers/alerts_mailer.rb
+++ app/mailers/alerts_mailer.rb
@@ -14,7 +14,11 @@ class AlertsMailer < ApplicationMailer
     @user = User.find(user_id)
     @count = tenders_count
     @tenders = Tender.where(ref_no: ref_nos_array)
-    @date = Time.now.in_time_zone('Asia/Singapore').to_date.yesterday
+    if Time.now.in_time_zone('Asia/Singapore').to_date.monday?
+      @date = "From #{Time.now.in_time_zone('Asia/Singapore').to_date - 3.days}"
+    else
+      @date = Time.now.in_time_zone('Asia/Singapore').to_date.yesterday
+    end
     mail(to: @user.email, subject: "Tengence Alerts #{@date}")
       # , :'X-MC-SendAt' => (Time.now.in_time_zone('Asia/Singapore') + 8.hours).utc.strftime("%Y-%m-%d %H:%M:%S"))
   end

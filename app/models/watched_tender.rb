4eef2f192c3cfa1ebaf9d96f6831e833d98808d0
Vic-L <vljc17@gmail.com>
2015-11-09 01:57:51 +0800

    shift order of association in watched_tenders so that tender is shown in rails admin under watched tender model

--- app/models/watched_tender.rb
+++ app/models/watched_tender.rb
@@ -1,7 +1,7 @@
 class WatchedTender < ActiveRecord::Base
+  belongs_to :tender, foreign_key: :tender_id
   belongs_to :current_tender, class_name: 'CurrentTender', foreign_key: :tender_id
   belongs_to :past_tender, class_name: 'PastTender', foreign_key: :tender_id
-  belongs_to :tender, foreign_key: :tender_id
   belongs_to :user
   validates_uniqueness_of :tender_id, scope: [:user]
 end

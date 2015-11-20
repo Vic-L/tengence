8c02d52adc92f8404d24c859fe96bbf0b6706e5b
Vic-L <vljc17@gmail.com>
2015-11-20 14:13:25 +0800

    tenders#new

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -2,6 +2,10 @@ class TendersController < ApplicationController
   before_action :store_location
   before_action :authenticate_user!
 
+  def new
+    @tender = Tender.new(ref_no: "InHouse-#{Time.now.to_formatted_s(:number)}", published_date: Date.today)
+  end
+
   def show
     @tender = Tender.find_by(ref_no: params[:id])
   end

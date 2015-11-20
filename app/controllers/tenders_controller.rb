c90a438c9fe7eecaacb03de6c80d35d8e6903ae4
Vic-L <vljc17@gmail.com>
2015-11-20 16:01:06 +0800

    successfully create new tender

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -3,7 +3,19 @@ class TendersController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @tender = Tender.new(ref_no: "InHouse-#{Time.now.to_formatted_s(:number)}", published_date: Date.today)
+    @tender = Tender.new(ref_no: "InHouse-#{Time.now.to_formatted_s(:number)}", published_date: Date.today, buyer_company_name: current_user.company_name)
+  end
+
+  def create
+    @tender = Tender.new tender_params
+    @tender.save
+    if @tender.persisted?
+      flash[:success] = "Tender Created Successfully!"
+      redirect_to current_tenders_path
+    else
+      flash[:alert] = @tender.errors.full_messages.to_sentence
+      redirect_to new_tender_path
+    end
   end
 
   def show
@@ -15,5 +27,19 @@ class TendersController < ApplicationController
       unless user_signed_in?
         store_location_for(:users, request.original_url)
       end
+    end
+
+    def tender_params
+      params.require(:tender).permit(
+        :ref_no,
+        :published_date,
+        :buyer_company_name,
+        :buyer_name,
+        :buyer_email,
+        :buyer_contact_number,
+        :budget,
+        :closing_datetime,
+        :description
+      )
     end
 end

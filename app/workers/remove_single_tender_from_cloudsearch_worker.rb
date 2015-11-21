6b104e1b9764d822353a72bd053b969f77191cc7
Vic-L <vljc17@gmail.com>
2015-11-21 16:46:53 +0800

    fix job to not refer to tender on destroy since its destroyed

--- app/workers/remove_single_tender_from_cloudsearch_worker.rb
+++ app/workers/remove_single_tender_from_cloudsearch_worker.rb
@@ -3,10 +3,9 @@ class RemoveSingleTenderFromCloudsearchWorker
   sidekiq_options :backtrace => true, :retry => false
 
   def perform ref_no
-    tender = Tender.find(ref_no)
     hash = {
       'type': "delete",
-      'id': tender.ref_no
+      'id': ref_no
     }
     AwsManager.upload_document [hash].to_json
     NotifyViaSlack.call(content: "<@ganther> Tender #{ref_no} removed from CloudSearch")

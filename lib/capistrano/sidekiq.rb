0336102f296d0052a35f8f83bc1ba9dfdf66f0b8
Vic-L <vljc17@gmail.com>
2015-11-23 13:01:38 +0800

    new task for execution during deployment

--- lib/capistrano/sidekiq.rb
+++ lib/capistrano/sidekiq.rb
@@ -13,3 +13,16 @@
 # :sidekiq_concurrency => nil
 set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
 set :sidekiq_queue, ['default', 'mailers']
+
+namespace :emailer do
+  desc 'Send out emailers'
+  task :schedule_send_keywords_tenders_emails do
+    on roles(:app) do
+      within release_path do
+        with :rails_env => fetch(:rails_env) do
+          execute :rake, "emailer:schedule_send_keywords_tenders_emails"
+        end
+      end
+    end
+  end
+end

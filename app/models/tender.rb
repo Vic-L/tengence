75450cf3313921ee556beb14d6851e33e717ed19
Vic-L <vljc17@gmail.com>
2015-11-29 10:49:07 +0800

    fix rails admin tender new form

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -22,4 +22,34 @@ class Tender < ActiveRecord::Base
   def remove_from_cloudsearch
     RemoveSingleTenderFromCloudsearchWorker.perform_async(self.ref_no)
   end
+
+  rails_admin do
+    edit do
+      field :ref_no
+      field :buyer_company_name
+      field :buyer_name
+      field :buyer_contact_number
+      field :buyer_email
+      field :description
+      field :published_date
+      field :closing_datetime
+      field :external_link do
+        help 'Leave BLANK for INHOUSE tenders'
+      end
+      field :status  do
+        help 'open; expired; cancelled'
+      end
+      field :category
+      field :remarks
+      field :budget do
+        help 'Leave BLANK for SCRAPED tenders'
+      end
+      field :postee_id do
+        help 'Leave BLANK for SCRAPED tenders; id of user who posting'
+      end
+      field :long_description do
+        help 'Leave BLANK for SCRAPED tenders'
+      end
+    end
+  end
 end

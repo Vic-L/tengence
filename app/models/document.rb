f76e8088ff21c4884f90b313dacf8a40498ecef0
Vic-L <vljc17@gmail.com>
2015-12-06 00:27:55 +0800

    validate file size 10MB

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,4 +3,5 @@ class Document < ActiveRecord::Base
 
   has_attached_file :upload
   do_not_validate_attachment_file_type :upload
+  validates_with AttachmentSizeValidator, attributes: :upload, less_than: 10.megabytes
 end

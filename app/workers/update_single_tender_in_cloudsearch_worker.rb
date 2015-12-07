e390b368eee16b45611897cfaf41d8691791f3f9
Vic-L <vljc17@gmail.com>
2015-12-07 22:14:12 +0800

    remove delete procedure during tender update

--- app/workers/update_single_tender_in_cloudsearch_worker.rb
+++ app/workers/update_single_tender_in_cloudsearch_worker.rb
@@ -4,12 +4,6 @@ class UpdateSingleTenderInCloudsearchWorker
 
   def perform ref_no, description
     hash = {
-      'type': "delete",
-      'id': ref_no
-    }
-    puts hash.to_s
-    AwsManager.upload_document [hash].to_json
-    hash = {
       'type': "add",
       'id': ref_no,
       'fields': {

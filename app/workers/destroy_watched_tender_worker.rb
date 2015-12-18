7a5e4f9c4f884abccaaa3ee4b1ec41be927fd6b5
Vic-L <vljc17@gmail.com>
2015-12-18 15:12:24 +0800

    destroy watch tender with reference to user_id

--- app/workers/destroy_watched_tender_worker.rb
+++ app/workers/destroy_watched_tender_worker.rb
@@ -2,7 +2,7 @@ class DestroyWatchedTenderWorker
   include Sidekiq::Worker
   sidekiq_options :backtrace => true, :retry => false
 
-  def perform tender_id
-    WatchedTender.find_by(tender_id: tender_id).destroy
+  def perform user_id, tender_id
+    WatchedTender.find_by(user_id: user_id,tender_id: tender_id).destroy
   end
 end

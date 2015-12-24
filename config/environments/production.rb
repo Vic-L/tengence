2f51f030340532117375e6af24b2e33a3204c184
Vic-L <vljc17@gmail.com>
2015-12-24 13:06:35 +0800

    change mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   # default host
-  config.action_mailer.default_url_options = { host: 'www.alerts.tengence.com.sg' }
+  config.action_mailer.default_url_options = { host: 'www.tengence.com.sg' }
 end

ae51feb275443729712fdddf98cb43215c4aae65
Vic-L <vljc17@gmail.com>
2015-12-26 17:52:08 +0800

    update default payment method token when updating payment method to use for subscription

--- app/controllers/brain_tree_controller.rb
+++ app/controllers/brain_tree_controller.rb
@@ -3,6 +3,7 @@ class BrainTreeController < ApplicationController
   before_action :deny_subscribed_user, only: [:subscribe]
 
   def billing
+    @payment_method = current_user.braintree_subscription.payment_method_token
   end
 
   def subscribe
@@ -11,9 +12,33 @@ class BrainTreeController < ApplicationController
 
   def edit_payment
     @client_token = Braintree::ClientToken.generate(customer_id: current_user.braintree_customer_id)
+    @payment_method = current_user.braintree_subscription.payment_method_token
   end
 
   def update_payment
+    result = Braintree::Subscription.update(current_user.braintree_subscription_id, payment_method_nonce: params['payment_method_nonce'])
+    
+    if result.success?
+      result = Braintree::PaymentMethod.update(
+        result.subscription.payment_method_token,
+        :options => {
+          :make_default => true
+        }
+      )
+      if result.success?
+        current_user.update(default_payment_method_token: result.payment_method.token)
+        flash[:success] = "You have successfully change your default payment method"
+        redirect_to billing_path
+      else
+        flash[:alert] = "An error occurred. Our developers are notified and are currently working on it. Thank you for your patience."
+        NotifyViaSlack.call(content: "<@vic-l> ERROR brain_tree#create_payment Braintree::PaymentMethod.create\r\n#{result}")
+        redirect_to :back
+      end
+    else
+      NotifyViaSlack.call(content: "<@vic-l> ERROR brain_tree#create_payment Braintree::Subscription.create\r\n#{result}")
+      binding.pry
+      redirect_to :back
+    end
   end
 
   def create_payment
@@ -39,12 +64,12 @@ class BrainTreeController < ApplicationController
         redirect_to billing_path
       else
         flash[:alert] = "An error occurred. Our developers are notified and are currently working on it. Thank you for your patience."
-        NotifyViaSlack.call(content: "<@vic-l> ERROR in braintree payment methods\r\n#{result}")
+        NotifyViaSlack.call(content: "<@vic-l> ERROR brain_tree#create_payment Braintree::Subscription.create\r\n#{result}")
         redirect_to :back
       end
     else
       flash[:alert] = "An error occurred. Our developers are notified and are currently working on it. Thank you for your patience."
-      NotifyViaSlack.call(content: "<@vic-l> ERROR in braintree payment methods\r\n#{result}")
+      NotifyViaSlack.call(content: "<@vic-l> ERROR brain_tree#create_payment Braintree::PaymentMethod.create\r\n#{result}")
       redirect_to :back
     end
   end

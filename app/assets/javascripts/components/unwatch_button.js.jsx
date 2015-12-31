0e3c2eeabf4e23aceb128c71dbaa2b1296ce8534
Vic-L <vljc17@gmail.com>
2015-12-31 18:03:01 +0800

    nest watch/unwatch buttons in div with id for watched_tenders#create/destroy ajax request to take effect

--- app/assets/javascripts/components/unwatch_button.js.jsx
+++ app/assets/javascripts/components/unwatch_button.js.jsx
@@ -1,7 +1,10 @@
 var UnwatchButton = React.createClass({
   render: function() {
+    var id = "watch-tender-" + this.props.refNo;
     return (
-      <a href={this.props.unwatchPath} data-remote='true' data-method='delete' className='button unwatch-button ga-tenders' data-gtm-category='' data-gtm-action='unwatch' data-gtm-label={this.props.refNo}>Unwatch</a>
+      <div id={id}>
+        <a href={this.props.unwatchPath} data-remote='true' data-method='delete' className='button unwatch-button ga-tenders' data-gtm-category='' data-gtm-action='unwatch' data-gtm-label={this.props.refNo}>Unwatch</a>
+      </div>
     );
   }
 });
--- app/assets/javascripts/components/watch_button.js.jsx
+++ app/assets/javascripts/components/watch_button.js.jsx
@@ -1,7 +1,10 @@
 var WatchButton = React.createClass({
   render: function() {
+    var id = "watch-tender-" + this.props.refNo;
     return (
-      <a href={this.props.watchPath} data-remote='true' data-method='post' className='button watch-button ga-tenders' data-gtm-category='' data-gtm-action='watch' data-gtm-label={this.props.refNo}>Watch</a>
+      <div id={id}>
+        <a href={this.props.watchPath} data-remote='true' data-method='post' className='button watch-button ga-tenders' data-gtm-category='' data-gtm-action='watch' data-gtm-label={this.props.refNo}>Watch</a>
+      </div>
     );
   }
 });

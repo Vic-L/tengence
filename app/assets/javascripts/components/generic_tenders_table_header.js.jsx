a06dd68e794ad074b5fec9c51ba1ed2c0df9477e
Vic-L <vljc17@gmail.com>
2016-01-01 14:20:42 +0800

    fix

--- app/assets/javascripts/components/generic_tenders_table_header.js.jsx
+++ app/assets/javascripts/components/generic_tenders_table_header.js.jsx
@@ -2,13 +2,15 @@ var GenericTendersTableHeader = React.createClass({
   render: function() {
     return (
       <thead>
-        <th className='medium-5'>Description</th>
-        <th className='medium-1'>Published Date</th>
-        <th className='medium-1'>Closing Date</th>
-        <th className='medium-1'>Closing Time</th>
-        <th className='medium-2'>Buyer Entity</th>
-        <th className='medium-1'>Watchlist</th>
-        <th className='medium-1'>Details</th>
+        <tr>
+          <th className='medium-5'>Description</th>
+          <th className='medium-1'>Published Date</th>
+          <th className='medium-1'>Closing Date</th>
+          <th className='medium-1'>Closing Time</th>
+          <th className='medium-2'>Buyer Entity</th>
+          <th className='medium-1'>Watchlist</th>
+          <th className='medium-1'>Details</th>
+        </tr>
       </thead>
     );
   }

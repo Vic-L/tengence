2539ff8a3e004911e63e6e0fd339e942a9b13d82
Vic-L <vljc17@gmail.com>
2016-01-06 01:29:41 +0800

    implement select_all check_box

--- app/assets/javascripts/components/watched_tenders_table_header.js.jsx
+++ app/assets/javascripts/components/watched_tenders_table_header.js.jsx
@@ -1,10 +1,17 @@
 var WatchedTendersTableHeader = React.createClass({
+  handleCheck: function(e){
+    if (e.target.checked) {
+      $("input[name^='select_single[]']").prop('checked', true);
+    } else {
+      $("input[name^='select_single[]']").prop('checked', false);
+    }
+  },
   render: function() {
     return (
       <thead>
         <tr>
           <th className="medium-1">
-            <input id="select_all" name="select_all" type="checkbox" value="1" />
+            <input id="select_all" name="select_all" type="checkbox" value="1" onClick={this.handleCheck} />
             <a id="destroy-all-button">
               <i className="fa fa-trash icon"></i>
             </a>

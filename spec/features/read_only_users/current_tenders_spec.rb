6d7258ecbec66c5b24585fadc0253b80116ef5e3
Vic-L <vljc17@gmail.com>
2016-01-26 18:20:07 +0800

    test for current_page without search

---
+++ spec/features/read_only_users/current_tenders_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+feature 'current_tenders', js: true, type: :feature do
+  let(:current_tenders_page) { CurrentTendersPage.new }
+  let(:read_only_user) {create(:user, :read_only)}
+
+  before :each do
+    Rails.application.load_seed
+    login_as(read_only_user, scope: :user)
+    current_tenders_page.visit_page
+    wait_for_page_load
+    page.driver.browser.manage.window.resize_to(1432, 782)
+  end
+  
+  scenario 'should display correct count' do
+    sleep 3
+    expect(current_tenders_page.find_css('span.total-count').first.all_text).to eq '102'
+    expect(current_tenders_page.find_css('span.total-count').last.all_text).to eq '102'
+    expect(current_tenders_page.find_css('.number-detail .number').first.all_text).to eq '102'
+    expect(current_tenders_page.find_css('.number-detail .number').last.all_text).to eq '62'
+  end
+
+  scenario 'should paginate correctly' do
+    descriptions = current_tenders_page.get_all_descriptions
+    current_tenders_page.find_css('nav.pagination a')[-2].click
+    wait_for_ajax
+    
+    descriptions << current_tenders_page.get_all_descriptions
+    current_tenders_page.find_css('nav.pagination a')[-2].click
+    wait_for_ajax
+    
+    descriptions << current_tenders_page.get_all_descriptions
+    
+    expect(descriptions.flatten.count).to eq descriptions.flatten.uniq.count
+  end
+
+  scenario 'should sort correctly' do
+    expect(current_tenders_page.find_css('select.sort').first.value).to eq 'newest'
+    expect(current_tenders_page.find_css('select.sort').last.value).to eq 'newest'
+    first_published_date = current_tenders_page.get_first_published_date
+    last_published_date = current_tenders_page.get_last_published_date
+    expect(first_published_date > last_published_date).to eq true
+
+    current_tenders_page.sort_by_expiring
+    wait_for_ajax
+
+    expect(current_tenders_page.find_css('select.sort').first.value).to eq 'expiring'
+    expect(current_tenders_page.find_css('select.sort').last.value).to eq 'expiring'
+
+    first_closing_datetime = (current_tenders_page.find_css('tbody tr td')[2].all_text + current_tenders_page.find_css('tbody tr td')[3].all_text).to_datetime
+    last_closing_datetime = (current_tenders_page.find_css('tbody tr:last-child td')[2].all_text + current_tenders_page.find_css('tbody tr:last-child td')[3].all_text).to_datetime
+    expect(first_closing_datetime < last_closing_datetime).to eq true
+
+    current_tenders_page.sort_by_newest
+    wait_for_ajax
+
+    expect(current_tenders_page.find_css('select.sort').first.value).to eq 'newest'
+    expect(current_tenders_page.find_css('select.sort').last.value).to eq 'newest'
+
+    expect(current_tenders_page.get_first_published_date).to eq first_published_date
+    expect(current_tenders_page.get_last_published_date).to eq last_published_date
+  end
+
+  scenario 'should show correct details for corresponding more button clicked' do
+  end
+
+  scenario 'should show time' do
+  end
+end
--- spec/support/page_objects/current_tenders_page.rb
+++ spec/support/page_objects/current_tenders_page.rb
@@ -9,4 +9,29 @@ class CurrentTendersPage
   def find_css selector
     page.driver.find_css selector
   end
+
+  def sort_by_newest
+    find_css('select.sort').first.find_xpath('option').first.click
+  end
+
+  def sort_by_expiring
+    find_css('select.sort').first.find_xpath('option').last.click
+  end
+
+  def get_first_published_date
+    find_css('tbody tr td')[1].all_text.to_date
+  end
+
+  def get_last_published_date
+    find_css('tbody tr:last-child td')[1].all_text.to_date
+  end
+
+  def get_all_descriptions
+    array = []
+    find_css('tbody tr').each do |tr|
+      array << tr.find_css('td')[0].all_text
+    end
+    array
+  end
+
 end

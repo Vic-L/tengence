8110264f09f2488feca50071380e0cab7e4eb8f2
Vic-L <vljc17@gmail.com>
2016-02-04 17:51:59 +0800

    fix to add to viewed_tender table when user_signed_in?

--- app/controllers/viewed_tenders_controller.rb
+++ app/controllers/viewed_tenders_controller.rb
@@ -1,6 +1,8 @@
 class ViewedTendersController < ApplicationController
   def create
-    ViewedTender.create(user_id: current_user.id, ref_no: params[:ref_no])
+    if user_signed_in?
+      ViewedTender.create(user_id: current_user.id, ref_no: params[:ref_no])
+    end
     render nothing: true
   end
 end

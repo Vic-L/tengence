9fce7bd7f3a687b620f5d966ccd2ce57d09dc53e
Vic-L <vljc17@gmail.com>
2016-02-05 16:07:54 +0800

    prevent non-unique creation of tender per user

--- app/controllers/trial_tenders_controller.rb
+++ app/controllers/trial_tenders_controller.rb
@@ -3,11 +3,11 @@ class TrialTendersController < ApplicationController
     if user_signed_in?
       tender = TrialTender.create(user_id: current_user.id, tender_id: params[:ref_no])
       if tender.persisted?
-        render json: current_user.trial_tender_ids.to_json
+        
       else
-        NotifyViaSlack.call(content: "<@vic-l> ERROR TrialTendersController#create\r\n#{current_user.email} trial tender #{params[:ref_no]}")
-        render json: 'ignore'.to_json  
+        NotifyViaSlack.call(content: "<@vic-l> ERROR TrialTendersController#create\r\n#{current_user.email} trial tender #{params[:ref_no]}\r\n#{tender.errors.full_messages.to_sentence}")
       end
+      render json: current_user.trial_tender_ids.to_json
     else
       render json: 'ignore'.to_json
     end
--- app/models/trial_tender.rb
+++ app/models/trial_tender.rb
@@ -1,4 +1,6 @@
 class TrialTender < ActiveRecord::Base
   belongs_to :user, counter_cache: true
   belongs_to :tender, foreign_key: :tender_id
+
+  validates :tender_id, uniqueness: { scope: :user_id, message: "already unlocked for the day for this user" }
 end

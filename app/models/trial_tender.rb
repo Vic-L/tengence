db707b6f49813ba78b1a2948a9001c21d0158d0b
Vic-L <vljc17@gmail.com>
2016-02-05 16:59:45 +0800

    improve trial tender model spec

--- app/models/trial_tender.rb
+++ app/models/trial_tender.rb
@@ -2,5 +2,5 @@ class TrialTender < ActiveRecord::Base
   belongs_to :user, counter_cache: true
   belongs_to :tender, foreign_key: :tender_id
 
-  validates :tender_id, uniqueness: { scope: :user_id, message: "already unlocked for the day for this user" }
+  validates :tender_id, uniqueness: { scope: :user_id }
 end
--- spec/models/trial_tender_spec.rb
+++ spec/models/trial_tender_spec.rb
@@ -3,4 +3,5 @@ require "spec_helper"
 feature TrialTender, type: :model do
   it { should belong_to(:user).counter_cache(true) }
   it { should belong_to(:tender) }
+  it { should validate_uniqueness_of(:tender_id).scoped_to(:user_id) }
 end

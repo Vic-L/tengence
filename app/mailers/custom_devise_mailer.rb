6e2a46c0d3beb76ad1e227870438e26862c0c3da
Vic-L <vljc17@gmail.com>
2016-02-15 21:39:06 +0800

    change smtp settings for devise mailer

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -2,8 +2,8 @@ class CustomDeviseMailer < Devise::Mailer
   self.smtp_settings = {
     :address => ENV['MAIL_ADDRESS'],
     :port => 587,
-    :user_name => ENV['MAIL_USERNAME'],
-    :password => ENV['MAIL_PASSWORD'],
+    :user_name => ENV['ADMIN_MAIL_USERNAME'],
+    :password => ENV['ADMIN_MAIL_PASSWORD'],
     :authentication => 'plain',
     enable_starttls_auto: true  }
 end

1d73a122a87fe9fd5dcce4d5ab349862b3e86adf
Vic-L <vljc17@gmail.com>
2016-04-11 21:04:48 +0800

    Revert "add URI.unescape for tenders#show"
    This reverts commit 5f96d1458827917a793bc98faefc719b7f79e8aa.

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -28,7 +28,7 @@ class TendersController < ApplicationController
   end
 
   def show
-    @tender = Tender.find_by(ref_no: URI.unescape(params[:id]))
+    @tender = Tender.find_by(ref_no: params[:id])
   end
 
   def edit

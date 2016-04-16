4117d3a09b87e0f683d5b2722e4cc92f6379c559
Vic-L <vljc17@gmail.com>
2016-04-16 21:25:05 +0800

    ping trial demos unlocked

--- app/controllers/trial_tenders_controller.rb
+++ app/controllers/trial_tenders_controller.rb
@@ -4,6 +4,7 @@ class TrialTendersController < ApplicationController
       if current_user.trial_tenders_count < 3
         tender = TrialTender.create(user_id: current_user.id, tender_id: params[:ref_no])
         if tender.persisted?
+          NotifyViaSlack.delay.call(content: "TrialTender UNLOCKED by #{current_user.email}: \r\n#{Tender.find(tender.tender_id).description}")
           render json: {statusCode: 'success',trial_tender_ids: current_user.trial_tender_ids}.to_json
         else
           NotifyViaSlack.delay.call(content: "<@vic-l> ERROR TrialTendersController#create\r\n#{current_user.email} trial tender #{params[:ref_no]}\r\n#{tender.errors.full_messages.to_sentence}")

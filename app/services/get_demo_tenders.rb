903eca701294b16493b059a7b489069888947716
Vic-l <vljc17@gmail.com>
2016-09-26 16:10:34 +0800

    fix methods that are left out during copy paste in GetDemoTenders service

--- app/services/get_demo_tenders.rb
+++ app/services/get_demo_tenders.rb
@@ -18,6 +18,14 @@ class GetDemoTenders
         eval("@tenders = #{table}.non_sesami.order(published_date: :desc).page(params['page']).per(10)")
         eval("@results_count = #{table}.count")
       end
+
+      @current_page = @tenders.current_page
+      @total_pages = @tenders.total_pages
+      @last_page = @tenders.last_page?
+      @tenders = @tenders.to_a
+      @watched_tender_ids = []
+
+      return [@tenders, @current_page, @total_pages, @last_page, @watched_tender_ids, @results_count]
     rescue => e
       NotifyViaSlack.delay.call(content: "<@vic-l> Error GetDemoTenders.rb\r\n#{e.message}\r\n#{e.backtrace.to_s}")
     end
